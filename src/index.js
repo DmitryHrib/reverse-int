4238cc2e5722e2009e93e1546609bc08addb987a
dmitry <Villigo701@gmail.com>
2022-01-19 11:48:58 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
+    let lev0 = Math.abs(n)
+    let lev = lev0.toString()
+    let i = 0;
+    result = '';
+    while (i < lev.length) {
+      result = `${lev[i]}${result}`;
+      i = i + 1;
+    }
+    return result;
+};
+
+
+
+
   
-}
+
+
+
+
+
+
